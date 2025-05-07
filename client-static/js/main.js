5130b002ca46d71fb96115847833d4e690ef7726
Kamal0414 <kamalcv0414@gmail.com>
2025-05-07 12:31:14 +0530

    initial commit

---
+++ client-static/js/main.js
@@ -0,0 +1,30 @@
+const products = [
+    {
+        id:1,
+        name:"Wireless Headphones",
+        price:2999,
+        image:"images/headphones.jpg",
+    },
+    {
+        id:2,
+        name:"Smart Watch",
+        price:4999 ,
+        image:"images/smartwatch.jpg",
+    },
+    {
+        id:3,
+        name:"Gaming Mouse",
+        price:1999,
+        image:"images/mouse.jpg",
+    }
+];
+const container = document.querySelector('.products-container');
+products.forEach(product => {
+    container.innerHTML += `
+    <div class="product-card">
+        <img src="${product.image}" alt="${product.name}" width="200"/>
+        <h3>${product.name}</h3>
+        <p>${product.price}</p>
+        <button>Add to Cart</button>
+        </div>
+        `;});
